d393c6ee29fd50fc9263d92a33bdce8082913e41
Jamilly <jamilly.danielle@gooxxy.com>
2024-05-29 00:07:48 -0300

    Incrementando menu de opções

--- program/menu.cpp
+++ program/menu.cpp
@@ -2,8 +2,54 @@
 
 using namespace std;
 
-void menu(){
-    cout<< "" << endl << "[ Analise de Grafos ]" << endl << endl;
+enum Menu {
+    MENU_PRINCIPAL,
+    MENU_VERIFICACAO,
+    MENU_LISTAGEM,
+    MENU_CONFIGURACOES
+};
+
+int qntOpcoes(Menu menu){  
+    int XOpcoes = 0;
+
+    switch(menu) {
+        case MENU_PRINCIPAL:
+            XOpcoes = 3;
+            break;
+        case MENU_VERIFICACAO:
+            XOpcoes = 8;
+            break;
+        case MENU_LISTAGEM:
+            XOpcoes = 8;
+            break;
+        case MENU_CONFIGURACOES:
+            XOpcoes = 10;
+            break;
+        default:
+            break;
+    }
+    return XOpcoes;
+}
+
+void navegacao(){
+    cout<< "Digite (v) para voltar"<< endl;
+
+}
+
+void menuPrincipal(){
+    cout<< "" << endl << "[ Menu principal ]" << endl << endl;
+    cout<< "* Selecione o Num. da opcao desejada:" << endl << endl;
+    cout<< "--------------------------------" << endl;
+    cout<< "Num. |   Secao" << endl;
+    cout<< "--------------------------------" << endl;
+    cout<< "1    |   Verificacao" << endl;
+    cout<< "2    |   Listagem" << endl;
+    cout<< "3    |   Configuracoes" << endl;
+    cout<< "--------------------------------" << endl << endl;
+}
+
+void menuVerificacao(){
+    cout<< "" << endl << "[ Analise de Grafo ]" << endl << endl;
     cout<< "* Selecione o Num. da opcao desejada:" << endl << endl;
     cout<< "--------------------------------" << endl;
     cout<< "Num. |   Tipo de verificacao" << endl;
@@ -17,11 +63,50 @@ void menu(){
     cout<< "7    |   Ciclico" << endl;
     cout<< "8    |   Planar" << endl;
     cout<< "--------------------------------" << endl << endl;
+    navegacao();
+}
+
+void menuListagem(){
+    cout<< "" << endl << "[ Listagem de atributos ]" << endl << endl;
+    cout<< "* Selecione o Num. da opcao desejada:" << endl << endl;
+    cout<< "--------------------------------" << endl;
+    cout<< "Num. |   Tipo de listagem" << endl;
+    cout<< "--------------------------------" << endl;
+    cout<< "1    |   Vertices" << endl;
+    cout<< "2    |   Arestas" << endl;
+    cout<< "3    |   Componentes conexas" << endl;
+    cout<< "4    |   Um caminho Euleriano" << endl;
+    cout<< "5    |   Um caminho Hamiltoniano" << endl;
+    cout<< "6    |   Hamiltoniano" << endl;
+    cout<< "7    |   Vertices de articulacao" << endl;
+    cout<< "8    |   Arestas ponte" << endl;
+    cout<< "--------------------------------" << endl << endl;
+    navegacao();
+}
+
+void menuConfiguracoes(){
+    cout<< "" << endl << "[ Gerar configuracoes ]" << endl << endl;
+    cout<< "* Selecione o Num. da opcao desejada:" << endl << endl;
+    cout<< "--------------------------------" << endl;
+    cout<< "Num. |   Tipo de configuracao" << endl;
+    cout<< "--------------------------------" << endl;
+    cout<< "1    |   Matriz de adjacencia" << endl;
+    cout<< "2    |   Lista de adjacencia" << endl;
+    cout<< "3    |   Arvore de profundidade" << endl;
+    cout<< "4    |   Arvore de largura" << endl;
+    cout<< "5    |   Arvore geradora minima" << endl;
+    cout<< "6    |   Ordem topologia" << endl;
+    cout<< "7    |   Vertices de articulacao" << endl; // Função não disponível em grafos não direcionado
+    cout<< "8    |   Caminho minimo entre dois vertices" << endl; // Função não disponível em grafos não ponderados
+    cout<< "9    |   Fluxo maximo" << endl; // Função não disponível em grafos não ponderados
+    cout<< "10   |   Fechamento transitivo" << endl; // Função não disponível em grafos não ponderados
+    cout<< "--------------------------------" << endl << endl;
+    navegacao();
 }
 
 int main(){
-
-    menu();
+    
+    menuPrincipal();
 
     return 0;
 }
