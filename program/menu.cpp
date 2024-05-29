c454f5605c15f3fb6b38dad0943c7bf50efd7eb4
Jamilly <jamilly.danielle@gooxxy.com>
2024-05-28 23:30:00 -0300

    Update no visual das opções (menu) - saída terminal

--- program/menu.cpp
+++ program/menu.cpp
@@ -3,18 +3,20 @@
 using namespace std;
 
 void menu(){
-    cout<< "Selecione o Num. da opção desejada:" << endl << endl;
-    cout<< "-------------------------------" << endl;
-    cout<< "Verificar..." << endl;
-    cout<< "-------------------------------" << endl << endl;
-    cout<< "1. Quantidade de vertices" << endl;
-    cout<< "2. Quantidade de arestas" << endl;
-    cout<< "3. Conexo" << endl;
-    cout<< "4. Bipartido" << endl;
-    cout<< "5. Euleriano" << endl;
-    cout<< "6. Hamiltoniano" << endl;
-    cout<< "7. Ciclico" << endl;
-    cout<< "8. Planar" << endl;
+    cout<< "" << endl << "[ Analise de Grafos ]" << endl << endl;
+    cout<< "* Selecione o Num. da opcao desejada:" << endl << endl;
+    cout<< "--------------------------------" << endl;
+    cout<< "Num. |   Tipo de verificacao" << endl;
+    cout<< "--------------------------------" << endl;
+    cout<< "1    |   Quantidade de vertices" << endl;
+    cout<< "2    |   Quantidade de arestas" << endl;
+    cout<< "3    |   Conexo" << endl;
+    cout<< "4    |   Bipartido" << endl;
+    cout<< "5    |   Euleriano" << endl;
+    cout<< "6    |   Hamiltoniano" << endl;
+    cout<< "7    |   Ciclico" << endl;
+    cout<< "8    |   Planar" << endl;
+    cout<< "--------------------------------" << endl << endl;
 }
 
 int main(){
