27aa401c3b1ddc197efde136237e0a7c928f1adb
Jamilly <jamilly.danielle@gooxxy.com>
2024-08-05 23:50:14 -0300

    corrigindo validação de arquivo

--- program/leituraArquivo.cpp
+++ program/leituraArquivo.cpp
@@ -125,6 +125,42 @@ bool ehBipartido(const vector<Aresta>& arestas, const vector<string>& vertices) 
     return true;
 }
 
+bool ehConexo(const vector<Aresta>& arestas, const vector<string>& vertices) {
+    map<string, vector<string>> adjacencia;
+    for (const auto& aresta : arestas) {
+        adjacencia[aresta.origem].push_back(aresta.destino);
+        adjacencia[aresta.destino].push_back(aresta.origem);
+    }
+
+    if (vertices.empty()) return true; // Grafo vazio é considerado conexo
+
+    set<string> visitados;
+    queue<string> fila;
+    fila.push(vertices[0]); // Começa com o primeiro vértice
+    visitados.insert(vertices[0]);
+
+    while (!fila.empty()) {
+        string u = fila.front();
+        fila.pop();
+
+        for (const auto& v : adjacencia[u]) {
+            if (visitados.find(v) == visitados.end()) {
+                visitados.insert(v);
+                fila.push(v);
+            }
+        }
+    }
+
+    // Verifica se todos os vértices foram visitados
+    for (const auto& vertice : vertices) {
+        if (visitados.find(vertice) == visitados.end()) {
+            return false; // Algum vértice não foi visitado, logo o grafo é desconexo
+        }
+    }
+
+    return true; // Todos os vértices foram visitados, logo o grafo é conexo
+}
+
 int main()
 {
 
@@ -167,10 +203,27 @@ int main()
 
                         posLeitura++; // Pula a vírgula -- incluir verificação de vírgula
                     }
+                    if (verificaFim(linha, posLeitura))
+                    {
+                        int posInicio = posLeitura;
+                        int intervalo;
+
+                        // Ler vértice
+                        while (linha[posLeitura] != '}')
+                        {
+                            posLeitura++;
+                        }
+                        intervalo = posLeitura - posInicio;
+                        string vertice = linha.substr(posInicio, intervalo);
+
+                        vertices.push_back(vertice);
+                        
+                        posLeitura = posLeitura + 2;
+                    }
 
                 }
 
-                posLeitura = posLeitura + 4;
+                posLeitura++;
 
                 if (linha[posLeitura] == 'A' and verificaInicio(linha, posLeitura))
                 {                      // Verificação de arestas
@@ -239,21 +292,30 @@ int main()
         cout << "Vertice: " << vertice << endl;
     }
 
+    cout << endl;
+
     // Exibe as arestas
     cout << "--- Arestas lidas ---" << endl;
     for (const auto &aresta : arestas)
     {
         cout << "Origem: " << aresta.origem
              << ", Destino: " << aresta.destino
-             << ", Peso: " << aresta.peso << endl;
+             << ", Peso: " << aresta.peso << endl << endl;
+    }
+
+    cout << "--- Verificacao - Grafo conexo? ---" << endl;
+    if (ehConexo(arestas, vertices)) {
+        cout << "Yes" << endl << endl;
+    } else {
+        cout << "No" << endl << endl;
     }
 
 
-    cout << "--- Verificacao de biparticao ---" << endl;
+    cout << "--- Verificacao - Grafo bipartido? ---" << endl;
     if (ehBipartido(arestas, vertices)) {
-        cout << "O grafo e bipartido." << endl;
+        cout << "Yes" << endl << endl;
     } else {
-        cout << "O grafo nao e bipartido." << endl;
+        cout << "No" << endl << endl;
     }
 
 
